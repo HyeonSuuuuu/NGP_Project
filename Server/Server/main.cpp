6826730cf072133e0654b76d2118a36b0c15ccc3
Dong-ho-nim <shindh758@gmail.com>
2025-11-26 23:59:13 +0900

    .

--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -23,7 +23,7 @@ int main()
 	g_obstacles = obstacles;
 	SOCKET listen_sock = NULL;
 	InitGlobals();
-	InitNetwork(&listen_sock);
+	InitNetwork(listen_sock);
 	HANDLE hThread = CreateThread(NULL, 0, AcceptThread, (LPVOID)listen_sock, 0, NULL);
 	// GameLoop
 	while (g_isRunning.load()) {
@@ -71,7 +71,7 @@ int main()
 	WSACleanup();
 }
 
-void InitNetwork(SOCKET *listen_sock)
+void InitNetwork(SOCKET listen_sock)
 {
 	int retval;
 	WSADATA wsa;
@@ -79,8 +79,8 @@ void InitNetwork(SOCKET *listen_sock)
 	if (retval != 0)
 		err_display(retval);
 
-	*listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (*listen_sock == INVALID_SOCKET)
+	listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET)
 		err_quit("listen socket()");
 
 	struct sockaddr_in serveraddr;
@@ -88,11 +88,11 @@ void InitNetwork(SOCKET *listen_sock)
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_addr.s_addr = INADDR_ANY;
 	serveraddr.sin_port = htons(SERVER_PORT);
-	int retVal = bind(*listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	int retVal = bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
 	if (retVal == SOCKET_ERROR)
 		err_quit("bind()");
 
-	retVal = listen(*listen_sock, SOMAXCONN);
+	retVal = listen(listen_sock, SOMAXCONN);
 	if (retVal == SOCKET_ERROR)
 		err_quit("listen()");
 }
